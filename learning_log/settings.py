6e1cfcec2d3a84bff320c5d86001ead67acfec75
caiweiyang1233.com <invest_cai@163.com>
2022-01-01 18:24:34 +0800

    my first web

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -15,6 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+INSTALLED_APPS=(
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # 我的应用程序
+    "learning_logs",
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
