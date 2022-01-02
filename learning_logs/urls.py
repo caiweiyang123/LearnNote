f7cbf45c31face26fd369ed75e8e662bbb95af9a
caiweiyang1233.com <invest_cai@163.com>
2022-01-02 22:38:43 +0800

    网站构建的第二天

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.index,name='index'),
+    # 主页
+    path('index/', views.index, name='index/'),
+    # 显示所有的主题
+    path('topics/', views.topics, name='topics/')
 ]
